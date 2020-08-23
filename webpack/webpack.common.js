6f77c08a6694098fb70ed5488e7ef5c58a9f288d
WesleyClements <wesley.clements@gmail.com>
2020-08-22 23:17:12 -0700

    formatted

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -11,15 +11,17 @@ module.exports = {
     globalObject: 'this',
   },
   module: {
-    rules: [{
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
       },
-    }, ],
+    ],
   },
 };
