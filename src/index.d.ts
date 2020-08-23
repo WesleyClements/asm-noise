52fe8dfc9b3a307e183b0585e4073467b6d67a93
Wesley Clements <wesley.clements@gmail.com>
2020-08-23 16:56:27 -0700

    switched to module definition

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,24 +4,26 @@
 
 export as namespace asmNoise;
 
-export = Noise;
+declare module asmNoise {
+  export = Noise;
 
-declare function Noise(x: number, y: number): number;
-declare function Noise(x: number, y: number, z: number): number;
-declare function Noise(x: number, y: number, z: number, z: number): number;
+  declare function Noise(x: number, y: number): number;
+  declare function Noise(x: number, y: number, z: number): number;
+  declare function Noise(x: number, y: number, z: number, z: number): number;
 
-declare namespace Noise {
-  export interface NoiseOffset {
-    x: number;
-    y: number;
-    z: number;
-    w: number;
+  declare namespace Noise {
+    export interface NoiseOffset {
+      x: number;
+      y: number;
+      z: number;
+      w: number;
+    }
+
+    export let algorithm: 'perlin' | 'open-simplex';
+    export let seed: number;
+    export let octaves: number;
+    export let lacunarity: number;
+    export let persistence: number;
+    export let offset: NoiseOffset;
   }
-
-  export let algorithm: 'perlin' | 'open-simplex';
-  export let seed: number;
-  export let octaves: number;
-  export let lacunarity: number;
-  export let persistence: number;
-  export let offset: NoiseOffset;
 }
