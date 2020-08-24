32be9822cc423c182700761820e612436025d18a
Wesley Clements <wesley.clements@gmail.com>
2020-08-24 04:50:57 -0700

    updated typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,28 +2,10 @@
 // Project: asm-noise
 // Definitions by: Wesley Clements <https://github.com/WesleyClements>
 
-export as namespace asmNoise;
+declare module 'asm-noise' {
+  function noise(x: number, y: number): number;
+  function noise(x: number, y: number, z: number): number;
+  function noise(x: number, y: number, z: number, w: number): number;
 
-declare module asmNoise {
-  export default Noise;
-
-  declare function Noise(x: number, y: number): number;
-  declare function Noise(x: number, y: number, z: number): number;
-  declare function Noise(x: number, y: number, z: number, z: number): number;
-
-  declare namespace Noise {
-    export interface NoiseOffset {
-      x: number;
-      y: number;
-      z: number;
-      w: number;
-    }
-
-    export let algorithm: 'perlin' | 'open-simplex';
-    export let seed: number;
-    export let octaves: number;
-    export let lacunarity: number;
-    export let persistence: number;
-    export let offset: NoiseOffset;
-  }
+  export default noise;
 }
