99d570bcef29e45dfc231f16af297f3d23d487b2
Wesley Clements <wesley.clements@gmail.com>
2020-11-21 18:18:01 -0800

    added config method

--- src/index.js
+++ src/index.js
@@ -1,62 +1,63 @@
 import perlin from './perlin.js';
 import openSimplexUnoptimized from './openSimplexUnoptimized.js';
 
-let noise;
+const algorithms = new Map([
+  ['perlin', perlin],
+  ['open-simplex', openSimplexUnoptimized],
+]);
 
-{
-  const algorithms = new Map([
-    ['perlin', perlin],
-    ['open-simplex', openSimplexUnoptimized],
-  ]);
+let algorithm = 'open-simplex';
+let octaves = 8;
+let lacunarity = (1 + Math.sqrt(5)) / 2;
+let persistence = Math.abs(1 - Math.sqrt(5)) / 2;
 
-  let algorithm = 'open-simplex';
-  let octaves = 8;
-  let lacunarity = (1 + Math.sqrt(5)) / 2;
-  let persistence = Math.abs(1 - Math.sqrt(5)) / 2;
+let offsetX = 5393 * lacunarity;
+let offsetY = 4691 * lacunarity;
+let offsetZ = 10093 * lacunarity;
+let offsetW = 9241 * lacunarity;
 
-  let offset;
+const offset = Object.defineProperties(
+  {},
   {
-    let offsetX = 5393 * lacunarity;
-    let offsetY = 4691 * lacunarity;
-    let offsetZ = 10093 * lacunarity;
-    let offsetW = 9241 * lacunarity;
-    offset = {
-      get x() {
-        return offsetX;
-      },
-      set x(value) {
+    x: {
+      get: () => offsetX,
+      set(value) {
         if (typeof value !== 'number') throw TypeError('must be a number');
         offsetX = value;
       },
-      get y() {
-        return offsetY;
-      },
-      set y(value) {
+    },
+    y: {
+      get: () => offsetY,
+      set(value) {
         if (typeof value !== 'number') throw TypeError('must be a number');
         offsetY = value;
       },
-      get z() {
-        return offsetZ;
-      },
+    },
+    z: {
+      get: () => offsetZ,
       set z(value) {
         if (typeof value !== 'number') throw TypeError('must be a number');
         offsetZ = value;
       },
-      get w() {
-        return offsetW;
-      },
+    },
+    w: {
+      get: () => offsetW,
       set w(value) {
         if (typeof value !== 'number') throw TypeError('must be a number');
         offsetW = value;
       },
-    };
+    },
   }
+);
 
-  noise = function (x, y, z, w) {
+export default Object.defineProperty(
+  function noise(x, y, z, w) {
     if (arguments.length < 2) return;
     switch (arguments.length) {
       case 2:
-        return algorithms.get(algorithm).noise2D(octaves, lacunarity, persistence, offset.x, offset.y, x, y);
+        return algorithms
+          .get(algorithm)
+          .noise2D(octaves, lacunarity, persistence, offset.x, offset.y, x, y);
       case 3:
         return algorithms
           .get(algorithm)
@@ -64,14 +65,43 @@ let noise;
       default:
         return algorithms
           .get(algorithm)
-          .noise4D(octaves, lacunarity, persistence, offset.x, offset.y, offset.z, offset.w, x, y, z, w);
+          .noise4D(
+            octaves,
+            lacunarity,
+            persistence,
+            offset.x,
+            offset.y,
+            offset.z,
+            offset.w,
+            x,
+            y,
+            z,
+            w
+          );
     }
-  };
-
-  Object.defineProperties(noise, {
+  },
+  {
+    config: {
+      value(options) {
+        if (options == null) return;
+        const { seed, algorithm, octaves, lacunarity, persistence, offset } = options;
+        if (seed != null) {
+        }
+        if (algorithm != null) {
+        }
+        if (octaves != null) {
+        }
+        if (lacunarity != null) {
+        }
+        if (persistence != null) {
+        }
+        if (offset != null) {
+        }
+      },
+    },
     algorithm: {
       get: () => algorithm,
-      set: (value) => {
+      set(value) {
         if (typeof value !== 'string') throw TypeError('algorithm must be a string');
         value = value.toLowerCase();
         if (!algorithms.has(value)) throw Error(`invalid algorithm: ${value}`);
@@ -80,13 +110,13 @@ let noise;
     },
     seed: {
       get: () => algorithms.get(algorithm).seed,
-      set: (value) => {
+      set(value) {
         algorithms.get(algorithm).seed = value;
       },
     },
     octaves: {
       get: () => octaves,
-      set: (value) => {
+      set(value) {
         if (typeof value !== 'number') throw TypeError('octave must be a number');
         if (value < 1) throw RangeError('octave must greater than 0');
         octaves = value;
@@ -94,7 +124,7 @@ let noise;
     },
     lacunarity: {
       get: () => lacunarity,
-      set: (value) => {
+      set(value) {
         if (typeof value !== 'number') throw TypeError('lacunarity must be a number');
         if (value === 0) throw RangeError('lacunarity must not be 0');
         lacunarity = value;
@@ -110,15 +140,47 @@ let noise;
     },
     offset: {
       get: () => offset,
-      set: (value) => {
-        if (typeof value != 'object') throw TypeError('offset must be a number');
+      set(value) {
+        if (typeof value != 'object') throw TypeError('offset must be a object');
         if (value.x != null) offset.x = value.x;
         if (value.y != null) offset.y = value.y;
         if (value.z != null) offset.z = value.z;
         if (value.w != null) offset.w = value.w;
       },
     },
-  });
-}
+  }
+);
 
-export default noise;
+let offset;
+{
+  offset = {
+    get x() {
+      return offsetX;
+    },
+    set x(value) {
+      if (typeof value !== 'number') throw TypeError('must be a number');
+      offsetX = value;
+    },
+    get y() {
+      return offsetY;
+    },
+    set y(value) {
+      if (typeof value !== 'number') throw TypeError('must be a number');
+      offsetY = value;
+    },
+    get z() {
+      return offsetZ;
+    },
+    set z(value) {
+      if (typeof value !== 'number') throw TypeError('must be a number');
+      offsetZ = value;
+    },
+    get w() {
+      return offsetW;
+    },
+    set w(value) {
+      if (typeof value !== 'number') throw TypeError('must be a number');
+      offsetW = value;
+    },
+  };
+}
