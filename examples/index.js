f19298a8c830afd55d1110bc95de6f45166804e0
Wesley Clements <wesley.clements@gmail.com>
2020-11-28 12:03:41 -0800

    added noise rendering

--- examples/index.js
+++ examples/index.js
@@ -1,9 +1,11 @@
 (function () {
-  const canvas = document.querySelector('canvas');
-  const ui = document.querySelector('#ui');
+  const main = document.querySelector('main');
+  const canvas = main.querySelector('canvas');
+  const ui = main.querySelector('#ui');
   const algorithmSelect = ui.querySelector('#algorithm-select');
   const dimensionSelect = ui.querySelector('#dimension-select');
   const seedInput = ui.querySelector('input[name=seed]');
+  const generateBtn = ui.querySelector('button');
 
   const updateCanvasDimensions = () =>
     void (canvas.width = canvas.height = innerWidth > innerHeight ? innerHeight : innerWidth);
@@ -11,11 +13,43 @@
   const configNoise = () =>
     noise.config({ algorithm: algorithmSelect.value, seed: seedInput.value });
 
+  let rendering = false;
   const renderNoise = () => {
-    const ctx = canvas.getContext('2d');
-    const imgData = ctx.createImageData(canvas.width, canvas.height);
+    if (rendering) return;
+    rendering = true;
 
-    ctx.putImageData(imgData, 0, 0);
+    generateBtn.classList.add('is-loading', 'disabled');
+
+    // disable config inputs while rendering
+    algorithmSelect.setAttribute('disabled', true);
+    dimensionSelect.setAttribute('disabled', true);
+    seedInput.setAttribute('disabled', true);
+
+    // wait on a timeout to allow browser to do it's stuff
+    new Promise((resolve) => setTimeout(() => resolve(), 0)).then(() => {
+      const ctx = canvas.getContext('2d');
+      const imgData = ctx.createImageData(canvas.width, canvas.height);
+      for (let i = 0; i < imgData.width; ++i) {
+        for (let j = 0; j < imgData.height; ++j) {
+          const index = (i + j * imgData.width) * 4;
+          const value = 255 * noise(i / 100, j / 100);
+          imgData.data[index + 0] = value;
+          imgData.data[index + 1] = value;
+          imgData.data[index + 2] = value;
+          imgData.data[index + 3] = 255;
+        }
+      }
+      ctx.putImageData(imgData, 0, 0);
+
+      // re-enble config inputs after rendering
+      algorithmSelect.removeAttribute('disabled');
+      dimensionSelect.removeAttribute('disabled');
+      seedInput.removeAttribute('disabled');
+
+      generateBtn.classList.remove('is-loading', 'disabled');
+
+      rendering = false;
+    });
   };
 
   seedInput.value = noise.seed;
@@ -23,13 +57,10 @@
   window.addEventListener('resize', () => updateCanvasDimensions());
   (function () {
     let configNoiseHandle;
-    let renderNoiseHandle;
 
     algorithmSelect.addEventListener('change', () => {
       clearTimeout(configNoiseHandle);
       configNoise();
-      clearTimeout(renderNoiseHandle);
-      renderNoise();
     });
     seedInput.addEventListener('keydown', () => {
       clearTimeout(configNoiseHandle);
@@ -39,11 +70,10 @@
           return;
         }
         configNoise();
-        clearTimeout(renderNoiseHandle);
-        renderNoiseHandle = setTimeout(() => renderNoise(), 300);
       }, 300);
     });
   })();
+  generateBtn.addEventListener('click', renderNoise);
 
   updateCanvasDimensions();
 })();
