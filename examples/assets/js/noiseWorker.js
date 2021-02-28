703c919340311d54c6551923006016fde3d93f7f
Wesley Clements <wesley.clements@gmail.com>
2021-02-28 12:05:01 -0800

    fixed seed and algorithm not being sent to worker

--- examples/assets/js/noiseWorker.js
+++ examples/assets/js/noiseWorker.js
@@ -2,7 +2,8 @@ importScripts('https://unpkg.com/asm-noise@1.0.2/dist/asm-noise.js');
 importScripts('./noiseImageGeneration.js');
 
 addEventListener('message', (ev) => {
-  const { scale, resolution, width, height } = ev.data;
+  const { seed, algorithm, scale, resolution, width, height } = ev.data;
+  noise.config({seed, algorithm});
   const { dt, noiseValues, imgData } = generateNoiseImageSync({ scale, resolution, width, height });
   console.log(dt);
   postMessage({ dt, noiseValues, imgData }, [noiseValues.buffer, imgData.data.buffer]);
