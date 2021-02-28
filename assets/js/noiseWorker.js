526ad6e3a4db07f88ea46726eabe47e701923bfb
Wesley Clements <wesley.clements@gmail.com>
2021-02-28 12:13:22 -0800

    Updates

--- assets/js/noiseWorker.js
+++ assets/js/noiseWorker.js
@@ -2,9 +2,9 @@ importScripts('https://unpkg.com/asm-noise@1.0.2/dist/asm-noise.js');
 importScripts('./noiseImageGeneration.js');
 
 addEventListener('message', (ev) => {
-  const { seed, algorithm, scale, resolution, width, height } = ev.data;
+  const { seed, algorithm, ...settings } = ev.data;
   noise.config({seed, algorithm});
-  const { dt, noiseValues, imgData } = generateNoiseImageSync({ scale, resolution, width, height });
+  const { dt, noiseValues, imgData } = generateNoiseImageSync(settings);
   console.log(dt);
   postMessage({ dt, noiseValues, imgData }, [noiseValues.buffer, imgData.data.buffer]);
 });
